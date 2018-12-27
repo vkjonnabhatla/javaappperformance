127725e74f3ae0a07164895c73f5e672027f4ffb
Venkatakrishna Jonnabatla <venkatakrishna_jonnabatla@gap.com>
2018-12-27 12:29:58 +0530

    Initial commit

---
+++ src/main/java/com/java/performance/javaappperformance/JavaappperformanceApplication.java
@@ -0,0 +1,14 @@
+package com.java.performance.javaappperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaappperformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaappperformanceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/java/performance/javaappperformance/JavaappperformanceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.java.performance.javaappperformance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaappperformanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
